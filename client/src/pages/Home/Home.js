749ba1a7729f18928b99ab30b298cb114be79246
jon <jonlyeatman@gmail.com>
2021-06-15 19:45:56 -0400

    First run at home page layout

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,8 +3,16 @@ import './style.scss'
 
 function Home() {
   return (
-    <div>
-        <h1>Home</h1>
+    <div className="home-background">
+      <div className="ppc-logo-bg">
+        <h1 className="ppc-text-logo">SHAW PPC <br/>DESIGN</h1>
+      </div>
+
+      <p className="home-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit elit vitae augue et. Et phasellus ut nam consectetur odio rhoncus tortor. Nunc metus gravida bibendum morbi. Lectus et ultrices habitasse consequat. Placerat cum pharetra, cursus sit commodo, gravida.
+         Varius adipiscing vitae sed dolor quam amet, vitae. Fermentum amet fringilla nibh sagittis. Risus integer tincidunt sodales nec, pulvinar volutpat nunc, ipsum. 
+      </p>
+
+      <button className="create-account-btn">Create AN ACCOUNT</button>
 
     </div>
   
