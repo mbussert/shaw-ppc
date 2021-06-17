db167cb6d81a65f9cc8fcae0bb4427fdc6c39dec
Alexandra Murawski <almurawski15@gmail.com>
2021-06-17 19:30:43 -0400

    created video and facts components

---
+++ client/src/components/facts/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Facts() {
+    return (
+        <div>
+            <h1>Q & A</h1>
+        </div>
+        );
+    }
+
+export default Facts;
---
+++ client/src/components/videoSection/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VideoSection () {
+    return (
+        <div>
+            <h1>How to Use</h1>
+        </div>
+        );
+    }
+
+export default VideoSection;
