e693c6310e98048e6f0022b1cc3fbfc6790f3c8b
Valerie Russell <vruss14@gmail.com>
2021-06-17 20:56:17 -0400

    fixed error with calculator POST request

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,25 @@
-
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    saveUser: function(userData) {
-        return axios.post('/api/users', userData);
-    }
+  // Get all orders
+  getOrders: function() {
+    return axios.get("/api/orders");
+  },
+  // Get the order with the given id
+  getOrder: function(id) {
+    return axios.get("/api/orders" + id);
+  },
+  // Save an order to the database
+  // This hasn't been working correctly with the proxy (in the client package.json)
+  saveOrder: function(orderData) {
+    return axios.post("/api/orders", orderData, {
+        headers: {
+            'Content-Type': 'application/json' 
+        }
+    });
+  },
+  // Saves a user when an account is created
+  saveUser: function(userData) {
+    return axios.post('/api/users', userData);
 }
+};
