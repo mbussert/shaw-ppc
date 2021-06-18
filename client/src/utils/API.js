a9865ef0a56a096ce7bc32b711d8121cf2c706db
Heather Storseth <heatherstorseth3@gmail.com>
2021-06-17 21:06:59 -0400

    in progress

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,5 +4,9 @@ import axios from 'axios';
 export default {
     saveUser: function(userData) {
         return axios.post('/api/users', userData);
+    },
+
+    getUser: function(userData) {
+        return axios.get('/api/users', userData);
     }
 }
