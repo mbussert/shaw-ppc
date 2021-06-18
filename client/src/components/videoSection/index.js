99721b08d9656994cc5db667f73218d8945403ba
Alexandra Murawski <almurawski15@gmail.com>
2021-06-17 20:16:42 -0400

    added holder elements to video section component

--- client/src/components/videoSection/index.js
+++ client/src/components/videoSection/index.js
@@ -4,8 +4,16 @@ function VideoSection () {
     return (
         <div>
             <h1>How to Use</h1>
+
+            <VideoSection>
+                INSERT VIDEO HERE 
+            </VideoSection>
+
+            <h2>Brief Title</h2>
+            <p>This is be a brief description of the walkthrough video.</p>
+
         </div>
-        );
-    }
+            );
+        }
 
 export default VideoSection;
