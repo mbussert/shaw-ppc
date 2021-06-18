9f966edda65007e0ae4b76d378e2dcb01437edd1
Brandon G.M <brandongmerritt@gmail.com>
2021-06-17 22:07:01 -0400

    fourohfour styling 01

--- client/src/components/NotFound/index.js
+++ client/src/components/NotFound/index.js
@@ -3,11 +3,13 @@ import React, { Component } from "react";
 class NotFound extends Component {
   render() {
     return (
+    <main className="fourohfour">
       <body>
         <div>
-          <h1 className="header-text">404 Error</h1>
+          <h1 className="jumbotron">404 Error</h1>
         </div>
       </body>
+      </main>
     );
   }
 }
