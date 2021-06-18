e7d06ca89829e9f15bab2b1410b9a4192c6c9148
jon <jonlyeatman@gmail.com>
2021-06-17 20:53:22 -0400

    Primary Footer layout

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,14 +2,37 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
-      <br />
-      <br />
-      <br />
-      <footer id="footer-relative">
-        <h1>Footer</h1>
+    
+      <footer id="footer-relative" className="footer-background">
+        
+        <div className="footer-line"></div>
+
+
+        <div className="footer-container">
+        
+          <div className="footer-card">
+
+          </div>
+          
+          <div className="footer-card">
+           <img src="" alt="Wall Pro Logo"/>
+           <h3>NEW PROJECT</h3>
+          </div>
+
+          <div className="footer-card">
+            <p>Sign up now for your free account so you can track all of your projects in one convenient place
+            </p>
+            <h3>SIGN UP</h3>
+          </div>
+
+          <div className="footer-card">
+             <img src="" alt="shaw ppc logo"/>
+          </div>
+        </div>
+
+
       </footer>
-    </div>
+    
   );
 }
 
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+// import axios from "axios";
 
-export default {
-  // Get all orders
-  getOrders: function() {
-    return axios.get("/api/orders");
-  },
-  // Get the order with the given id
-  getOrder: function(id) {
-    return axios.get("/api/orders" + id);
-  },
-  // Save an order to the database
-  // This hasn't been working correctly with the proxy (in the client package.json)
-  saveOrder: function(orderData) {
-    return axios.post("/api/orders", orderData, {
-        headers: {
-            'Content-Type': 'application/json' 
-        }
-    });
-  }
-};
+// export default {
+//   // Get all orders
+//   getOrders: function() {
+//     return axios.get("/api/orders");
+//   },
+//   // Get the order with the given id
+//   getOrder: function(id) {
+//     return axios.get("/api/orders" + id);
+//   },
+//   // Save an order to the database
+//   // This hasn't been working correctly with the proxy (in the client package.json)
+//   saveOrder: function(orderData) {
+//     return axios.post("/api/orders", orderData, {
+//         headers: {
+//             'Content-Type': 'application/json' 
+//         }
+//     });
+//   }
+// };
