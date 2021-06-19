5d8d84750b357cce0289a09f0f3d842abaa5f699
Heather Storseth <heatherstorseth3@gmail.com>
2021-06-19 13:39:09 -0400

    passport added, will expand on that

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const passport = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.post('/login', passport.authenticate('local', { successRedirect: '/',
-  failureRedirect: '/login' }));
+app.post('/login', 
+  passport.authenticate('local'), 
+  function(email, password, done) {
+    User.findOne({ email: email }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    })
+  });
 
 app.use(routes);
 
