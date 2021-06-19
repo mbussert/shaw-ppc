d9a9bb5a10bc11216be908715d37bc4e00d6cac5
Al Murawski <almurawski15@gmail.com>
2021-06-19 12:53:21 -0400

    installed react-player and adjusted video component

--- client/src/components/videoSection/index.js
+++ client/src/components/videoSection/index.js
@@ -1,13 +1,22 @@
 import React from "react";
+import ReactPlayer from 'react-player';
 
 function VideoSection () {
     return (
-        <div>
+        <section>
             <h1>How to Use</h1>
+                <div className='player-wrapper'>
+                <ReactPlayer
+                className='react-player'
+                url='https://www.youtube.com/watch?v=aFYsJYPye94'
+                width='100%'
+                height='100%'
+                />
+            </div>
             <h2>Brief Title</h2>
             <p>This will be a brief description of the walkthrough video.</p>
 
-        </div>
+        </section>
             );
         }
 
