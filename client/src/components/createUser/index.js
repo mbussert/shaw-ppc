a7b42ee78a04054ff49df63bf85e027ff0456525
Heather Storseth <heatherstorseth3@gmail.com>
2021-06-19 13:41:18 -0400

    code cleanup

--- client/src/components/createUser/index.js
+++ client/src/components/createUser/index.js
@@ -2,87 +2,88 @@ import React, { useState } from "react";
 import API from "../../utils/API";
 
 function CreateUser() {
-
   const [userObject, setUser] = useState({});
 
   function handleInputChange(e) {
     const { name, value } = e.target;
-    setUser({...userObject, [name]: value})
-  };
-
+    setUser({ ...userObject, [name]: value });
+  }
 
   function handleFormSubmit(e) {
     e.preventDefault();
     if (userObject) {
-      console.log('Yay!')
-      // 
+      console.log("Yay!");
       API.saveUser({
         first: userObject.first,
         last: userObject.last,
         email: userObject.email,
         pass1: userObject.pass1,
         pass2: userObject.pass2,
-        password: userObject.pass1 
-      })
+        password: userObject.pass1,
+      });
     } else {
-      console.log('Nooo!')
+      console.log("Nooo!");
     }
   }
 
-    return (
-      <form onSubmit={handleFormSubmit}>
-        <h2>Create an account</h2>
-        <div className="userFirst-div">
-          <label for="create-first">First Name:</label>
-          <input 
-          className="add-first" 
-          type="text" 
-          name='first'
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <h2>Create an account</h2>
+      <div className="userFirst-div">
+        <label for="create-first">First Name:</label>
+        <input
+          className="add-first"
+          type="text"
+          name="first"
           onChange={handleInputChange}
-          />
-        </div>
+        />
+      </div>
 
-        <div className="userLast-div">
-          <label for="create-last">Last Name:</label>
-          <input 
-          className="add-last" 
-          name='last'
+      <div className="userLast-div">
+        <label for="create-last">Last Name:</label>
+        <input
+          className="add-last"
+          name="last"
           onChange={handleInputChange}
-          type="text" />
-        </div>
+          type="text"
+        />
+      </div>
 
-        <div className="userEmail-div">
-          <label for="create-email">Email:</label>
-          <input 
-          className="add-email" 
-          name='email'
+      <div className="userEmail-div">
+        <label for="create-email">Email:</label>
+        <input
+          className="add-email"
+          name="email"
           onChange={handleInputChange}
-          type="text" />
-        </div>
+          type="text"
+        />
+      </div>
 
-        <div className="userPass-div">
-          <label for="create-pass">Password:</label>
-          <input 
-          className="add-pass" 
-          name='pass1'
+      <div className="userPass-div">
+        <label for="create-pass">Password:</label>
+        <input
+          className="add-pass"
+          name="pass1"
           onChange={handleInputChange}
-          type="password" />
-        </div>
+          type="password"
+        />
+      </div>
 
-        <div className="userPass-div-div">
-          <label for="pass-create-pass">Password:</label>
-          <input 
-          className="pass-add-pass" 
-          name='pass2'
+      <div className="userPass-div-div">
+        <label for="pass-create-pass">Password:</label>
+        <input
+          className="pass-add-pass"
+          name="pass2"
           onChange={handleInputChange}
-          type="password" />
-        </div>
+          type="password"
+        />
+      </div>
 
-        <button className="create-btn" type="submit">
-          Create an Account
-        </button>
-      </form>
-    );
-  }
+      <button className="create-btn" type="submit">
+        Create an Account
+      </button>
+    </form>
+  );
+}
 
 export default CreateUser;
