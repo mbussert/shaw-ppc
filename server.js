40b49a5ba40438206a5578945a717eac0370b15f
Valerie Russell <vruss14@gmail.com>
2021-06-19 21:24:26 -0400

    added session information for logged in users

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const passport = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
