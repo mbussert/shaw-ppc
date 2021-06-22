c4c7194254b00fd0f20def15a1f68a97dbd2c59b
Al Murawski <almurawski15@gmail.com>
2021-06-22 18:08:32 -0400

    updated fqa section on help page

--- client/src/components/facts/index.js
+++ client/src/components/facts/index.js
@@ -2,8 +2,19 @@ import React from "react";
 
 function Facts() {
     return (
-        <div>
-            <h1>Question and Answer</h1>
+        <div className="fact-section">
+            <h1 className="fact-title">Question and Answer</h1>
+            
+            <ul className="fqa-list">
+                <li> Instruction 1</li>
+                <li> Instruction 2</li>
+                <li> Instruction 3</li>
+            </ul>
+
+            <div className="image-container">
+                <img className="images" src="" alt="sc1" />
+                <img className="images" src="" alt="sc2" />
+            </div>
         </div>
         );
     }
