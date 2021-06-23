ac4e2216d2526a69c3613af1613ef44efcfa13c1
Mike Bussert <mbussert@gmail.com>
2021-06-23 16:48:40 -0400

    Adding styling to ProjectList component

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -1,9 +1,38 @@
 import React from "react";
+import {
+  Container,
+  ListItem,
+  Divider,
+  ListItemText,
+  List,
+  Paper,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 340,
+    backgroundColor: theme.palette.background.paper,
+    borderRadius: 5,
+  },
+}));
 
 function ProjectList() {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <h1>Project List</h1>
+      <List>
+        <ListItem button>
+          <ListItemText primary="Custom Wall 1" />
+        </ListItem>
+        <Divider />
+        <ListItem button>
+          <ListItemText primary="Custom Wall 2" />
+        </ListItem>
+      </List>
     </div>
   );
 }
