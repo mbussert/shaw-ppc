18ef4fdf5780d32554312302e48cdec89e7c1e68
Mike Bussert <mbussert@gmail.com>
2021-06-22 23:09:07 -0400

    Modified index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./assets/scss/main.css";
+import "./assets/scss/main.scss";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
