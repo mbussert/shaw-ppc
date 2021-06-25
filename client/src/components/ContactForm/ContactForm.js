76865ca673b3ab11f36cdf0c5bfbb696ea9458d1
jon <jonlyeatman@gmail.com>
2021-06-24 21:10:37 -0400

    Form set up

--- client/src/components/ContactForm/ContactForm.js
+++ client/src/components/ContactForm/ContactForm.js
@@ -1,7 +1,42 @@
+import React from 'react';
+
 function ContactForm() {
     return (
-        <div>
-            <h1>noooooo</h1>
+        <div className="contact-form-container">
+            <form className="contact-form">
+                <h2 className="contact-form-text">Contact US</h2>
+                <input 
+                    type="text" 
+                    name="fullName"
+                    placeholder="Full Name"
+                    className="contact-form-name" 
+                />
+                <input 
+                    type="email" 
+                    name="email"
+                    placeholder="Email"
+                    className="contact-form-email" 
+                />
+                <input 
+                    type="text" 
+                    name="subject"
+                    placeholder="Subject"
+                    className="contact-form-subject" 
+                />
+                <textarea 
+                    name="message"
+                    placeholder="Message" 
+                    className="contact-form-message" 
+                    cols="30" 
+                    rows="10"
+                    ></textarea>
+                <button 
+                    type="submit" 
+                    className="contact-form-submit"  
+                >Send Message</button>
+                
+            </form>
+            
         </div>
     )
 }
