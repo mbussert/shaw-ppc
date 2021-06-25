c252e41c0858a7e2d8fbc8e1fad736357620493b
Valerie Russell <vruss14@gmail.com>
2021-06-25 17:57:59 -0400

    commented out lines interfering with tests

--- client/src/components/NotFound/index.js
+++ client/src/components/NotFound/index.js
@@ -4,18 +4,21 @@ import { Link } from "react-router-dom";
 class NotFound extends Component {
   render() {
     return (
-    <html>
-    <main className="fourohfour">
-      <body>
-        <div>
-          <container className="container-card">
-          <h1 className="header-text">404 Error: Page Not Found</h1>
-          <button className="btn"><Link to="./">Go Back Home</Link></button>
-          </container>
-        </div>
-      </body>
-      </main>
-      </html>
+
+    // No need to include the lines commented out -- React should automatically add these when the page is displayed
+
+    // <html>
+    // <main className="fourohfour">
+    //   <body>
+        // <div>
+          <div className="container-card">
+            <h1 className="header-text">404 Error: Page Not Found</h1>
+            <button className="btn"><Link to="./">Go Back Home</Link></button>
+          </div>
+        // </div>
+      // </body>
+      // </main>
+      // </html>
     );
   }
 }
