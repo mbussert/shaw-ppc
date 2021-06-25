bfe0300bdb1f22a4c18c9d627a9e05408d9476cc
Valerie Russell <vruss14@gmail.com>
2021-06-25 18:04:40 -0400

    created basic tests for subcomponents and calculator

--- models/Wall.js
+++ models/Wall.js
@@ -3,9 +3,6 @@ const sequelize = require("../config/connection.js");
 
 class Wall extends Model {}
 
-// UserId foreign key is commented out temporarily while login is being created
-// Otherwise form submission will fail due to a lack of a User ID
-
 Wall.init(
   {
     id: {
