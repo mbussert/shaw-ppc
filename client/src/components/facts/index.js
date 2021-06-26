34da6e348a51737c29ca6a060969f4e3739542bc
Al Murawski <almurawski15@gmail.com>
2021-06-26 11:01:01 -0400

    added calculator button

--- client/src/components/facts/index.js
+++ client/src/components/facts/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Facts() {
     return (
@@ -15,7 +16,13 @@ function Facts() {
                 <img className="images" src="" alt="sc1" />
                 <img className="images" src="" alt="sc2" />
             </div>
+
+        <button className="calc-btn">
+            <Link to="./Calculator">Calculator</Link>
+        </button>
+        
         </div>
+
         );
     }
 
