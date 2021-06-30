ecc28ca031421767f2d2116f954f422c45367f35
jon <jonlyeatman@gmail.com>
2021-06-29 20:47:21 -0400

    Redesign of contact form

--- client/src/components/ContactForm/ContactForm.js
+++ client/src/components/ContactForm/ContactForm.js
@@ -28,40 +28,47 @@ function ContactForm() {
     return (
         <>
         <Navbar loginStatus={loginStatus} />
+
         <div className="contact-form-container">
             <h2 className="contact-form-text">Contact US</h2>
             <form className="contact-form">
+            <label htmlFor="contact-form-label-name">Full Name</label>
                 <input 
                     type="text" 
                     name="fullName"
-                    placeholder="Full Name"
+                    // placeholder="Full Name"
                     className="contact-form-name" 
                 />
+                <label htmlFor="contact-form-label-email">Email</label>
                 <input 
                     type="email" 
                     name="email"
-                    placeholder="Email"
+                    // placeholder="Email"
                     className="contact-form-email" 
                 />
+                <label htmlFor="contact-form-label-subject">Subject</label>
                 <input 
                     type="text" 
                     name="subject"
-                    placeholder="Subject"
+                    // placeholder="Subject"
                     className="contact-form-subject" 
                 />
+                <label htmlFor="contact-form-label-message">Message</label>
                 <textarea 
                     name="message"
-                    placeholder="Message" 
+                    // placeholder="Message" 
                     className="contact-form-message" 
                     // cols="30" 
                     rows="10"
-                    ></textarea>
-                <button 
-                    type="submit" 
-                    className="contact-form-submit"  
-                >Send Message</button>
+                ></textarea>
 
             </form>
+
+            <button 
+                type="submit" 
+                className="contact-form-submit">
+            Send Message
+            </button>
             
         </div>
         </>
