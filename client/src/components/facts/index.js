788883b8c46a51044cd0539df3f6a144da53c926
Al Murawski <almurawski15@gmail.com>
2021-07-02 13:10:05 -0400

    Updated q&a content and styling

--- client/src/components/facts/index.js
+++ client/src/components/facts/index.js
@@ -8,11 +8,13 @@ function Facts() {
 
             <ul className="fqa-list">
                 <li> What measurements am I able to enter with the tool? </li>
-                <li> Users are able to enter measurements in inches. Please calculate manually your measurements from feet to inches to continue calculation of orders.</li>
+                <li> Users are able to enter measurements in inches. Please calculate manually your measurements from feet to inches to continue calculation of orders.
+                    <br/> Distance in inches = (distance in feet) x (12 inches/foot)
+                </li>
                 <li> Can I calculate more than one wall in an order? </li>
                 <li> Users may calculate multiple wall measurements all under one project name/order.</li>
                 <li> Can I use the calculator tool without creating an account with WallPro?</li>
-                <li> Users are not required to create an account to use the calculator tool but it is suggested to reference previous calculations.</li>
+                <li> Users are not required to create an account to use the calculator tool however we strongly suggest you do to reference previous calculations.</li>
             </ul>
 
             <div className="image-container">
