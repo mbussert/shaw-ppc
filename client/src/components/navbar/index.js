c567356e7e1d1afa83b2e245c611ee32d72a5e88
Brandon G.M <brandongmerritt@gmail.com>
2021-07-01 20:19:30 -0500

    navbarburgers02

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import ReactDOM from "react-dom";
 import API from "../../utils/API";
@@ -41,40 +41,82 @@ function Navbar(props) {
       .catch((err) => console.log(err));
   }
 
+  const [open, setOpen] = React.useState(false);
+  // const node = React.useRef();
   return (
     <div>
-    <div className="navLogo">
-          <Link to="./"><img className="navImg" src="/images/wall-pro-logo.png"/></Link>
-    <nav>
-      <ul className="links">
-        <li>
-          <Link to="./How">How it Works</Link>
-        </li>
-        <li>
-          <Link to="./Calculator">Calculator</Link>
-        </li>
-        {showAccount()}
-        {/* <li>
-          <Link to="./Account">Account</Link>
-        </li> */}
-        <li>{verifyStatus()}</li>
-        {/* <li>
-          <Link to="./Create">Create</Link>
-        </li>
-        <li>
-          <Link to="./About">About</Link>
-        </li> */}
-        <div>
-          <div className="line"></div>
-        </div>
-      </ul>
-    </nav>
-    </div>
-    <div className="hamburgerMQ">
-    <GiHamburgerMenu/>
-    </div>
+      <div className="navLogo">
+        <Link to="./"><img className="navImg" src="/images/wall-pro-logo.png" /></Link>
+        <nav>
+          <ul className="links">
+            <li>
+              <Link to="./How">How it Works</Link>
+            </li>
+            <li>
+              <Link to="./Calculator">Calculator</Link>
+            </li>
+            {showAccount()}
+            {/* <li>
+              <Link to="./Account">Account</Link>
+            </li> */}
+            <li>{verifyStatus()}</li>
+            {/* <li>
+              <Link to="./Create">Create</Link>
+            </li>
+            <li>
+              <Link to="./About">About</Link>
+            </li> */}
+            <div className="line"></div>
+          </ul>
+        </nav>
+      </div>
+      
+      
+      <div className="hamburgerMQ">
+        <GiHamburgerMenu open={open} setOpen={setOpen}/>
+      </div>
+      <nav className="mobileNav">
+          <ul className="mobileLinks">
+            <li>
+              <Link to="./How">How it Works</Link>
+            </li>
+            <li>
+              <Link to="./Calculator">Calculator</Link>
+            </li>
+            {showAccount()}
+            {/* <li>
+              <Link to="./Account">Account</Link>
+            </li> */}
+            <li>{verifyStatus()}</li>
+            {/* <li>
+              <Link to="./Create">Create</Link>
+            </li>
+            <li>
+              <Link to="./About">About</Link>
+            </li> */}
+          </ul>
+        </nav>
     </div>
   );
+}
+
+const Menu = ({ open }) => {
+  return (
+    <StyledMenu open={open}>
+      <a href="/">
+        <span role="img" aria-label="about us">💁🏻‍♂️</span>
+        About us
+      </a>
+      <a href="/">
+        <span role="img" aria-label="price">💸</span>
+        Pricing
+        </a>
+      <a href="/">
+        <span role="img" aria-label="contact">📩</span>
+        Contact
+        </a>
+    </StyledMenu>
+  )
 }
 
 export default Navbar;
