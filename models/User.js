e691cf22ddf67c95dc47f3615021b9d9ec235da6
Valerie Russell <vruss14@gmail.com>
2021-07-06 11:42:43 -0400

    updated models with validation & new hook

--- models/User.js
+++ models/User.js
@@ -19,7 +19,10 @@ User.init(
     email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true
+        unique: true,
+        validate: {
+          isEmail: true
+        }
     },
     password: {
       type: DataTypes.STRING,
